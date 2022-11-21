88e1470cff49d7a6b4b32471122f45ed9df92fbd
Fabian Schillig <xorgmc@gmail.com>
2022-11-21 20:23:39 +0100

    Removed default.nix, modified default jumpcutter.py for cpu transcoding

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# fast-jumpcutter
+(1 minute video)
+python3 jumpcutter-orig.py --input_file VL1a.mp4 --frame_rate 5 --sample_rate  15,43s user 1,53s system 268% cpu 6,316 total
+python3 jumpcutter.py --input_file VL1a.mp4 --silent_speed 99999  0.1  0  11,23s user 2,96s system 265% cpu 5,350 total
+
+
+
 # jumpcutter
 Automatically edits videos. Explanation here: https://www.youtube.com/watch?v=DQ8orIurGxw
 
@@ -20,6 +27,3 @@ I have processed 17-minute videos completely fine, but be wary if you're gonna g
 I want to use pyinstaller to turn this into an executable, so non-techy people
 can use it EVEN IF they don't have Python and all those libraries. Jabrils 
 recommended this to me. However, my pyinstaller build did not work. :( HELP
-
-## Building with nix
-`nix-build` to get a script with all the libraries and ffmpeg, `nix-build -A bundle` to get a single binary.
--- jumpcutter.py
+++ jumpcutter.py
@@ -139,7 +139,7 @@ createPath(TEMP_FOLDER)
 
 if(args.prefps != None):
     print("Pre-converting file to", args.prefps, "FPS...")
-    subprocess.call("ffmpeg -hide_banner -loglevel error -y -i \""+INPUT_FILE+"\" -filter:v fps="+str(int(args.prefps))+" -c:v h264_nvenc -b:v 5M -c:a copy \""+TEMP_FOLDER+"/temp.mp4\"", shell=True)
+    subprocess.call("ffmpeg -hide_banner -loglevel error -y -i \""+INPUT_FILE+"\" -filter:v fps="+str(int(args.prefps))+" -c:v libx264 -preset veryfast -c:a copy \""+TEMP_FOLDER+"/temp.mp4\"", shell=True)
     INPUT_FILE=TEMP_FOLDER+"temp.mp4"
     frameRate=args.prefps
 
@@ -274,7 +274,7 @@ wavfile.write(TEMP_FOLDER+"/audioNew.wav",SAMPLE_RATE,outputAudioData)
 
 print("Joining frames and audio to mp4 file...")
 
-command = "ffmpeg -hide_banner -loglevel error -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -c:v mjpeg_cuvid -framerate "+str(frameRate)+" -i "+TEMP_FOLDER+"/newFrame%06d.jpg -i "+TEMP_FOLDER+"/audioNew.wav -c:a aac -c:v h264_nvenc "+OUTPUT_FILE
+command = "ffmpeg -hide_banner -loglevel error -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -c:v mjpeg_cuvid -framerate "+str(frameRate)+" -i "+TEMP_FOLDER+"/newFrame%06d.jpg -i "+TEMP_FOLDER+"/audioNew.wav -c:a aac -c:v libx264 "+OUTPUT_FILE
 subprocess.call(command, shell=True)
 
 if not os.path.ismount(TEMP_FOLDER):
